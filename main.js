8309415e42e1fd7081529ef93b42d2b88623e4a9
Joseph Carter <josephc@Josephs-MacBook-Air.local>
2023-07-01 11:24:19 -0400

    working functionality

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-const wrapper = document.getElementById('wrapper')
-
+// for submit button of the form to enter information
 function submitGame() {
-    
+    const form = document.querySelector('.wrapper');
+    //event to handle to form 
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        // create varibles
+        const classname = ["game_title", "company_title", "price", "stock_options"];
+        //grab error message
+        const error = document.querySelector("#errorMessage");
+        //ping = for controller
+        let ping = true
+        let errmessages = [];
+        const allData = [];
+        allData.push(event.target.game_title.value);
+        allData.push(event.target.company_title.value);
+        allData.push(event.target.price.value);
+        allData.push(event.target.stock_options.value);
+        //fires off error message if any field of the form is not filled out
+        
+    })
 }
