f2f61a58689d92b6c01e798d94b70380f96d7ceb
Joseph Carter <josephc@josephs-air.mynetworksettings.com>
2023-07-01 15:03:21 -0400

    fixed improper uses of varibles, methods, and access points and added proper files this time

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
 // for submit button of the form to enter information
-function submitGame() {
-    const form = document.querySelector('.wrapper');
+    const form = document.querySelector('#form');
     //event to handle to form 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         // create varibles
-        const classname = ["gameTitle", "companyTitle", "price", "stockOptions"];
+        const className = ["gameTitle", "companyTitle", "price", "stockOptions"];
         //grab error message
-        const error = document.querySelector("#errorMessage");
-        let errmessages = [];
-        const allData = [];
-        allData.push(event.target.game_title.value);
-        allData.push(event.target.company_title.value);
-        allData.push(event.target.price.value);
-        allData.push(event.target.stock_options.value);
+        const errorDisplay = document.querySelector("#errorMessage");
+        let errMessages = [];
+        const formData = {
+            gameTitle: event.target.gameTitle.value,
+            companyTitle: event.target.companyTitle.value,
+            price: event.target.price.value,
+            stockOptions: event.target.stockOptions.value,
+        };
+        console.log(formData)
         //fires off error message if any field of the form is not filled out
-        for (let data of allData) {
-            if (!data) {
-                errmessages.push("Please fill out the entire form in order to proceed.")
-                ping = false
+        for (let data in formData) {
+            if (!formData[data]) {
+                errMessages.push("Please fill out the entire form in order to proceed.")
                 break;
             }
         }
         //checking if price is an actual number 
-        if (Number(allData[2]) < 0) {
-            errmessages.push("Please enter a real price point.");
+        if (Number(formData.price) < 0) {
+            errMessages.push("Please enter a real price point.");
             event.target.price.value = 0;
         }
         // Checking to see if numbers were actually entered and not characters
-        if(!/^["|']{0,1}[-]{0,1}\d{0,}(\.{0,1}\d+)["|']{0,1}$/.test(arrData[2]) && !/\d+/.test(arrData[2])){
-            errMsg.push("Character's not accpted please entre real numbers.");
+        if(!/^["|']{0,1}[-]{0,1}\d{0,}(\.{0,1}\d+)["|']{0,1}$/.test(formData.price) && !/\d+/.test(formData.price)){
+            errMessages.push("Character's not accpted please entre real numbers.");
             event.target.price.value = "";
         }
-        if(flag) {
-            // erase error msg block 
-            errmessages.textContent = "";
-            errmessages.setAttribute("style","display:none;")
-        }
+        console.log(errorDisplay, errMessages)
+        errMessages.forEach((error) => {
+            console.log('creating error')
+            let errorListItem = document.createElement('li');
+            errorListItem.innerText = error
+            errorDisplay.append(errorListItem);
+        })
     })
-}
