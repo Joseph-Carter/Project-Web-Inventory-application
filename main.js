9529dc1fff757506e0cb61cfbb3b3945526ff180
Joseph Carter <josephc@josephs-air.mynetworksettings.com>
2023-07-02 00:49:45 -0400

    finished product

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-// for submit button of the form to enter information
+
 const form = document.querySelector("#form");
-//event to handle to form
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  // create varibles
+
   const className = [
     "gameTitle",
     "companyTitle",
     "price",
     "stockOptions, removeButton",
   ];
-  //grab error message
+
   const errorDisplay = document.querySelector("#errorMessage");
-  errorDisplay.style.background = 'transparent'
+  errorDisplay.style.background = "transparent";
   errorDisplay.innerText = "";
   let errMessages = [];
   const formData = {
@@ -22,19 +22,19 @@ form.addEventListener("submit", (event) => {
     price: event.target.price.value,
     stockOptions: event.target.stockOptions.value,
   };
-  //fires off error message if any field of the form is not filled out
+ 
   for (let data in formData) {
     if (!formData[data]) {
       errMessages.push("Please fill out the entire form in order to proceed.");
       break;
     }
   }
-  //checking if price is an actual number
+
   if (Number(formData.price) < 0) {
     errMessages.push("Please enter a real price point.");
     event.target.price.value = 0;
   }
-  // Checking to see if numbers were actually entered and not characters
+
   if (
     !/^["|']{0,1}[-]{0,1}\d{0,}(\.{0,1}\d+)["|']{0,1}$/.test(formData.price) &&
     !/\d+/.test(formData.price)
@@ -43,14 +43,14 @@ form.addEventListener("submit", (event) => {
     event.target.price.value = "";
   }
   if (errMessages.length) {
-    errorDisplay.style.background = 'rgba(236, 95, 95, 0.192)'
+    errorDisplay.style.background = "rgba(236, 95, 95, 0.192)";
     errMessages.forEach((error) => {
       let errorListItem = document.createElement("li");
       errorListItem.innerText = error;
       errorDisplay.append(errorListItem);
     });
   } else {
-    //creates listed items
+
     const gameList = document.querySelector("#gameHolder");
     const formListItem = document.createElement("li");
     const gameNameTitle = document.createElement("h2");
@@ -81,5 +81,17 @@ form.addEventListener("submit", (event) => {
     );
     formListItem.append(gameContainer);
     gameList.append(formListItem);
+  }
+});
+
+let inStock = true;
+let stockHolder = document.querySelector(".stockHolder");
+let stockButton = document.getElementById("changeStock");
+stockButton.addEventListener("click", (event) => {
+  inStock = !inStock;
+  if (inStock) {
+    stockHolder.innerText = "In Stock";
+  } else {
+    stockHolder.innerText = "Out Of Stock";
   }
 });
